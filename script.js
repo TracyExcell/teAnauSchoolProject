368fd06ac6b3697b9fd5792125906eb89bd25f7d
TracyExcell <excelltracy@gmail.com>
2018-07-29 17:50:36 +1200

    First Draft

---
+++ script.js
@@ -0,0 +1,18 @@
+$(".button1").click(function(){
+    $(".overlay1").show();
+    
+});
+
+
+$(".button2").click(function(){
+    $(".overlay2").show();
+});
+
+$(".button3").click(function(){
+    $(".overlay3").show();
+});
+    
+$(".cross").click(function(){
+    $(".overlay").hide();
+});
+   
